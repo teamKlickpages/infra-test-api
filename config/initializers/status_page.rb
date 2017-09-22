1382aa3f9535fef6a1d0f0b635d5bc74374d0f5a
Rafael Rodrigues <rafaelrsantos.ti@gmail.com>
2017-09-22 16:36:00 -0300

    Altera a configuração do REDIS para utilizar variaveis de ambiente.

--- config/initializers/status_page.rb
+++ config/initializers/status_page.rb
@@ -1,5 +1,5 @@
 StatusPage.configure do
   self.interval = 10
   self.use :database
-  self.use :redis, url: 'redis://redis:6379/1'
+  self.use :redis, url: "redis://#{ENV['APP_REDIS_HOST']}:#{ENV['APP_REDIS_PORT']}/1"
 end
